1382d0e0b029f8b8931c66ba7b030f734ff580d8
MScott-Dev <MScott0199@gmail.com>
2024-03-12 14:18:29 -0400

    Server setup and typeDefs

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+import { ApolloServer } from 'apollo-server-express'
+import { typeDefs, resolvers } from './schemas' 
+import { authMiddleware } from './utils/auth'
 
 const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
@@ -14,8 +22,22 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client'))
+})
+
+// Function to make a new instanec of Apollo server
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+}
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  });
+
+  // Call function to start server
+startApolloServer();
